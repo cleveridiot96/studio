3130f62f7d5519fa72a1b10feb3dab02e8a2386a
Mira Parmar <miramasala1@gmail.com>
2025-07-06 09:52:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarInset clas

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -144,7 +144,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
               </div>
             </header>
             <LoadingBarInternal />
-            <SidebarInset className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8 w-full print:p-0 print:m-0 print:overflow-visible flex flex-col">
+            <SidebarInset className="flex-1 overflow-y-auto p-4 w-full print:p-0 print:m-0 print:overflow-visible flex flex-col">
               <ErrorBoundary>
                 <div className="flex flex-col flex-1 w-full min-w-0">
                     {children}
