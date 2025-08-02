0f7828afa455ae698d3670f985a38be3981c5c42
Mira Parmar <miramasala1@gmail.com>
2025-08-02 17:30:24 +0000

    make the entire thing 100% offline, becayse when i load it offline it us

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
-
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   title: "Kisan Khata Sahayak",
@@ -17,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning={true}>
-      <body className={cn("bg-background font-sans antialiased", fontSans.variable)}>
+      <body className={cn("bg-background font-sans antialiased")}>
         <div className="h-full w-full">
           {children}
         </div>
